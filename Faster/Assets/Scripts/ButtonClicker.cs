380120ceb5127ac34f27e0984d5794fdd1a5c6b9
foryourselfand <foryourselfand@gmail.com>
2018-12-03 15:13:32 +0300

    Growing Button

---
+++ Faster/Assets/Scripts/ButtonClicker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClicker : MonoBehaviour
+{
+    public GameObject activeButton;
+    private GameObject destroyObject;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseDown()
+    {
+        destroyObject = (GameObject) Instantiate(activeButton, transform.position, Quaternion.identity);
+    }
+
+    void OnMouseUp()
+    {
+        DestroyObject(destroyObject);
+    }
+}
---
+++ Faster/Assets/Scripts/GrowingButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowingButton : MonoBehaviour
+{
+	public float speed;
+	public Vector3 target = new Vector3(1, 1, 1);
+	
+	// Use this for initialization
+	void Start () {
+		transform.localScale = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = Vector3.MoveTowards (transform.localScale, target, Time.deltaTime * speed);
+	}
+}
